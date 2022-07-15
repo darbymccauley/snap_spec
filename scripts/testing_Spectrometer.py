f134ade5c26526cc26d003f87d2800fca17e2b10
darbymccauley <darbymccauley@berkeley.edu>
2022-07-15 11:21:54 -0700

    adding script to test leuschner.py

---
+++ scripts/testing_Spectrometer.py
@@ -0,0 +1,14 @@
+import numpy as np
+import sys 
+import os
+sys.path.append(os.path.abspath('../src'))
+from leuschner import Spectrometer
+
+# Open IPython to continue session as desired
+import IPython
+Q = input("\n\nDo you wish to continue in IPython? [y/n] ")
+if Q == "y":
+    print("\nOpening IPython...:")
+    IPython.embed()
+elif Q == "n":
+    print("Goodbye.")
