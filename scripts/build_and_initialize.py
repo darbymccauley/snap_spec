e41913ddb744ff1d71bca4d92361cdd011645a37
darbymccauley <darbymccauley@berkeley.edu>
2022-08-08 10:11:39 -0700

    add snap selection argument

--- scripts/build_and_initialize.py
+++ scripts/build_and_initialize.py
@@ -7,8 +7,10 @@ import argparse
 from leuschner import Spectrometer
 
 parser = argparse.ArgumentParser(description='Run spectrometer.')
+parser.add_argument('snap', types=str, help='which snap to configure to')
 parser.add_argument('fpga', type=str, help='fpg file')
 args = parser.parse_args()
+SNAP = args.snap
 FPGA = args.fpga
 
 fpga = casperfpga.CasperFpga('localhost')
@@ -16,6 +18,10 @@ fpga.upload_to_ram_and_program(FPGA)
 s = SnapFengine('localhost', transport='default')
 
 spec = Spectrometer(fpgfile=FPGA)
-spec.initialize()
+
+if SNAP is None:
+    spec.initialize()
+elif SNAP is 'discover':
+    spec.initialize_discover_SNAP(chipsel=[0])
 
 import IPython; IPython.embed()
