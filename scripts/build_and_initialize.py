e06b30fc084ad2684e9a9ba429e6a38b71f9b19b
Darby McCauley <darbymccauley@berkeley.edu>
2022-08-24 22:04:11 +0100

    changing parser args for build_and_initialize script

--- scripts/build_and_initialize.py
+++ scripts/build_and_initialize.py
@@ -4,27 +4,21 @@ import casperfpga
 from hera_corr_f import SnapFengine
 import argparse
 
-from leuschner import Discover_Spectrometer, Spectrometer
+from leuschner import Spectrometer
 
 parser = argparse.ArgumentParser(description='Run spectrometer.')
-parser.add_argument('snap', type=str, help='which snap to configure to')
-parser.add_argument('fpga', type=str, help='fpg file')
+parser.add_argument('fpga', type=str, help='fpg file to program')
+parser.add_argument('is_discover', type=bool, help='Is the discover SNAP being used?')
+
 args = parser.parse_args()
-SNAP = args.snap
 FPGA = args.fpga
+DISCOVER = args.is_discover
 
 fpga = casperfpga.CasperFpga('localhost')
 fpga.upload_to_ram_and_program(FPGA)
 s = SnapFengine('localhost', transport='default')
 
-
-if SNAP == 'Default':
-    spec = Spectrometer(fpgfile=FPGA)
-    spec.initialize()
-elif SNAP == 'Discover':
-    spec = Discover_Spectrometer(fpgfile=FPGA)
-    spec.initialize()
-else:
-    raise IOError('Snap argument not recognized. Please specify SNAP type: Default or Discover.')
+spec = Spectrometer(is_discover=DISCOVER)
+spec.initialize()
 
 import IPython; IPython.embed()
