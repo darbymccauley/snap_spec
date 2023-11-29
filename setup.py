9af380f718d01cded02353152130a223fb912385
Darby McCauley <97992525+darbymccauley@users.noreply.github.com>
2023-11-29 15:08:39 -0800

    Update setup.py
    Minor updates to description and url

--- setup.py
+++ setup.py
@@ -7,11 +7,11 @@ import glob
 
 if __name__ == '__main__':
     setup(name = 'snap_spec',
-        description = 'Leuschner radio spectrometer control software.',
+        description = 'Control software for the UC Berkeley UGRadio lab SNAP spectrometer and correlator.',
         long_description = __doc__,
         author = 'Darby McCauley',
         author_email = 'darbymccauley@berkeley.edu',
-        url = 'https://github.com/darbymccauley/Leuschner_Spectrometer',
+        url = 'https://github.com/darbymccauley/snap_spec',
         packages = ['snap_spec'],
         package_dir = {'snap_spec':'src'},
         package_data = {'snap_spec': ['fpga/ugradio_corrspec_2022-02-22_0905.fpg']},
